6565811a54cf0cbc3a5688f9978c49b99d0d5c6e
William Jojarth <150176606+wjojarth23@users.noreply.github.com>
2024-03-01 16:46:48 +0000

    Initial commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+ import { Button } from "flowbite-react";
+export function HomePage() {
+
+  return (
+    <div>
+              
+      <div className="bg-white dark:bg-gray-800 text-black dark:text-white">
+        <div className="text-xl text-center my-3">Home Page</div>
+        <div>
+          Welcome to DraftApp, your smart writing companion! To get started, make an account and edit a document.
+        </div>
+      </div>
+    </div>
+  );
+}
